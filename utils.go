89cc609ccf023002cc03f33dcd0632e2b05b2807
Riley Flynn <riley@rileyflynn.me>
2021-07-08 04:35:32 -0230

    Distinguish between single and plural authors & add source link to quote, where applicable

--- utils.go
+++ utils.go
@@ -8,7 +8,7 @@ import (
 	"github.com/nint8835/scribe/database"
 )
 
-func GenerateAuthorString(authors []*database.Author, guildID string) (string, error) {
+func GenerateAuthorString(authors []*database.Author, guildID string) (string, string, error) {
 	authorNames := []string{}
 
 	for _, author := range authors {
@@ -16,7 +16,7 @@ func GenerateAuthorString(authors []*database.Author, guildID string) (string, e
 		if guildID != "" {
 			user, err := Bot.GuildMember(guildID, author.ID)
 			if err != nil {
-				return "", fmt.Errorf("error getting guild member %s: %w", author.ID, err)
+				return "", "", fmt.Errorf("error getting guild member %s: %w", author.ID, err)
 			}
 
 			if user.Nick != "" {
@@ -27,7 +27,7 @@ func GenerateAuthorString(authors []*database.Author, guildID string) (string, e
 		} else {
 			user, err := Bot.User(author.ID)
 			if err != nil {
-				return "", fmt.Errorf("error getting user %s: %w", author.ID, err)
+				return "", "", fmt.Errorf("error getting user %s: %w", author.ID, err)
 			}
 
 			name = fmt.Sprintf("%s#%s", user.Username, user.Discriminator)
@@ -35,24 +35,40 @@ func GenerateAuthorString(authors []*database.Author, guildID string) (string, e
 		authorNames = append(authorNames, name)
 	}
 
-	return strings.Join(authorNames, ", "), nil
+	var label string
+	if len(authors) == 1 {
+		label = "Author"
+	} else {
+		label = "Authors"
+	}
+
+	return strings.Join(authorNames, ", "), label, nil
 }
 
 func MakeQuoteEmbed(quote *database.Quote, guildID string) (*discordgo.MessageEmbed, error) {
-	authors, err := GenerateAuthorString(quote.Authors, guildID)
+	authors, authorLabel, err := GenerateAuthorString(quote.Authors, guildID)
 	if err != nil {
 		return &discordgo.MessageEmbed{}, fmt.Errorf("error getting quote authors: %w", err)
+	}
+
+	fields := []*discordgo.MessageEmbedField{
+		{
+			Name:  authorLabel,
+			Value: authors,
+		},
+	}
+
+	if quote.Source != nil {
+		fields = append(fields, &discordgo.MessageEmbedField{
+			Name:  "Source",
+			Value: *quote.Source,
+		})
 	}
 
 	return &discordgo.MessageEmbed{
 		Title:       fmt.Sprintf("Quote %d", quote.Meta.ID),
 		Description: quote.Text,
 		Color:       (80 << 16) + (40 << 8) + (200),
-		Fields: []*discordgo.MessageEmbedField{
-			{
-				Name:  "Author(s)",
-				Value: authors,
-			},
-		},
+		Fields:      fields,
 	}, nil
 }
