b3f0114fce66d73d285654f7eda1ec7f45fd312d
Riley Flynn <riley@rileyflynn.me>
2023-11-16 19:51:53 -0330

    Forbid quoting a message with no content

--- commands.go
+++ commands.go
@@ -73,6 +73,23 @@ func AddQuoteCommand(_ *discordgo.Session, interaction *discordgo.InteractionCre
 }
 
 func AddQuoteMessageCommand(_ *discordgo.Session, interaction *discordgo.InteractionCreate, message *discordgo.Message) {
+	if message.Content == "" {
+		Bot.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Embeds: []*discordgo.MessageEmbed{
+					{
+						Color:       (240 << 16) + (85 << 8) + (125),
+						Title:       "Error adding quote.",
+						Description: "You cannot quote an empty message.",
+					},
+				},
+				Flags: discordgo.MessageFlagsEphemeral,
+			},
+		})
+		return
+	}
+
 	quoteUrl := fmt.Sprintf("https://discord.com/channels/%s/%s/%s", message.GuildID, message.ChannelID, message.ID)
 
 	quote := database.Quote{
