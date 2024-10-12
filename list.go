8c1a58dd10a29da47b00c178ab66ea4480450295
Riley Flynn <riley@rileyflynn.me>
2024-10-12 12:03:22 -0230

    Consolidate search and list into a single command

--- list.go
+++ list.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"gorm.io/gorm/clause"
@@ -11,10 +12,21 @@ import (
 
 type ListArgs struct {
 	Author *discordgo.User `description:"Author to display quotes for. Omit to display quotes from all users."`
+	Query  *string         `description:"Optional keyword / phrase to search for."`
 	Page   int             `default:"1" description:"Page of quotes to display."`
 }
 
 func ListQuotesCommand(_ *discordgo.Session, interaction *discordgo.InteractionCreate, args ListArgs) {
+	if result := database.Instance.Exec("PRAGMA case_sensitive_like = OFF", nil); result.Error != nil {
+		Bot.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("Error enabling case-insensitive like.\n```\n%s\n```", result.Error),
+			},
+		})
+		return
+	}
+
 	var quotes []database.Quote
 
 	query := database.Instance.Model(&database.Quote{}).Preload(clause.Associations)
@@ -23,6 +35,17 @@ func ListQuotesCommand(_ *discordgo.Session, interaction *discordgo.InteractionC
 		query = query.
 			Joins("INNER JOIN quote_authors ON quote_authors.quote_id = quotes.id").
 			Where(map[string]interface{}{"quote_authors.author_id": args.Author.ID})
+	}
+
+	if args.Query != nil {
+		queryString := *args.Query
+		if strings.Contains(queryString, "%") {
+			query = query.Where("text LIKE ?", queryString)
+		} else {
+			query = query.Where("text LIKE ?", "% "+queryString+" %").
+				Or("text LIKE ?", "% "+queryString).
+				Or("text LIKE ?", queryString)
+		}
 	}
 
 	result := query.Limit(5).Offset(5 * (args.Page - 1)).Find(&quotes)
