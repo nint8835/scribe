0cfcb94f38953dbbb39e8b82e01aaa6fd408f170
Riley Flynn <riley@rileyflynn.me>
2025-08-30 15:33:37 -0230

    Global least seen method

--- pkg/selection/first.go
+++ pkg/selection/first.go
@@ -45,6 +45,38 @@ func firstQuoteLeastSeen(ctx context.Context, userId string) (database.Quote, er
 	return quote, nil
 }
 
+func firstQuoteLeastSeenGlobal(ctx context.Context, userId string) (database.Quote, error) {
+	var quote database.Quote
+	db := database.Instance.WithContext(ctx)
+
+	err := db.Raw(
+		`SELECT
+			q.*,
+			COUNT(DISTINCT ca.id) + COUNT(DISTINCT cb.id) AS comparison_count
+		FROM
+			quotes q
+			LEFT JOIN completed_comparisons ca ON ca.quote_a_id = q.id
+			LEFT JOIN completed_comparisons cb ON cb.quote_b_id = q.id
+		WHERE
+			q.deleted_at IS NULL
+		GROUP BY
+			q.id
+		ORDER BY
+			comparison_count ASC,
+			random()
+		LIMIT
+		1`,
+		userId,
+		userId,
+	).Take(&quote).Error
+
+	if err != nil {
+		return database.Quote{}, err
+	}
+
+	return quote, nil
+}
+
 func firstQuoteRandom(ctx context.Context, _ string) (database.Quote, error) {
 	var quote database.Quote
 	db := database.Instance.WithContext(ctx)
@@ -95,9 +127,10 @@ func firstQuoteHighestElo(ctx context.Context, _ string) (database.Quote, error)
 }
 
 const (
-	FirstQuoteMethodLeastSeen  FirstQuoteMethod = "least_seen"
-	FirstQuoteMethodRandom     FirstQuoteMethod = "random"
-	FirstQuoteMethodHighestElo FirstQuoteMethod = "highest_elo"
+	FirstQuoteMethodLeastSeen       FirstQuoteMethod = "least_seen"
+	FirstQuoteMethodLeastSeenGlobal FirstQuoteMethod = "least_seen_global"
+	FirstQuoteMethodRandom          FirstQuoteMethod = "random"
+	FirstQuoteMethodHighestElo      FirstQuoteMethod = "highest_elo"
 )
 
 func (m FirstQuoteMethod) String() string {
@@ -108,6 +141,8 @@ func (m FirstQuoteMethod) DisplayName() string {
 	switch m {
 	case FirstQuoteMethodLeastSeen:
 		return "Least seen"
+	case FirstQuoteMethodLeastSeenGlobal:
+		return "Least seen (global)"
 	case FirstQuoteMethodRandom:
 		return "Random"
 	case FirstQuoteMethodHighestElo:
@@ -121,6 +156,8 @@ func (m FirstQuoteMethod) Description() string {
 	switch m {
 	case FirstQuoteMethodLeastSeen:
 		return "Selects the quote that you have completed the least comparisons for."
+	case FirstQuoteMethodLeastSeenGlobal:
+		return "Selects the quote that has had the least comparisons completed for, across all users."
 	case FirstQuoteMethodRandom:
 		return "Selects a quote completely at random."
 	case FirstQuoteMethodHighestElo:
@@ -132,14 +169,16 @@ func (m FirstQuoteMethod) Description() string {
 
 var FirstQuoteMethods = []FirstQuoteMethod{
 	FirstQuoteMethodLeastSeen,
+	FirstQuoteMethodLeastSeenGlobal,
 	FirstQuoteMethodRandom,
 	FirstQuoteMethodHighestElo,
 }
 
 var firstQuoteSelectors = map[FirstQuoteMethod]FirstQuoteSelector{
-	FirstQuoteMethodLeastSeen:  firstQuoteLeastSeen,
-	FirstQuoteMethodRandom:     firstQuoteRandom,
-	FirstQuoteMethodHighestElo: firstQuoteHighestElo,
+	FirstQuoteMethodLeastSeen:       firstQuoteLeastSeen,
+	FirstQuoteMethodLeastSeenGlobal: firstQuoteLeastSeenGlobal,
+	FirstQuoteMethodRandom:          firstQuoteRandom,
+	FirstQuoteMethodHighestElo:      firstQuoteHighestElo,
 }
 
 func selectFirstQuote(ctx context.Context, userId string, method FirstQuoteMethod) (database.Quote, error) {
