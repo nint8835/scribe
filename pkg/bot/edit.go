9e4323060212692ec747db31482fae05090b73a3
Riley Flynn <riley@rileyflynn.me>
2025-08-30 21:46:40 -0230

    Update embedding on edit

--- pkg/bot/edit.go
+++ pkg/bot/edit.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/nint8835/scribe/pkg/config"
 	"github.com/nint8835/scribe/pkg/database"
+	"github.com/nint8835/scribe/pkg/embedding"
 )
 
 type editArgs struct {
@@ -52,7 +53,31 @@ func (b *Bot) editQuoteCommand(_ *discordgo.Session, interaction *discordgo.Inte
 		return
 	}
 
-	//TODO: Update embedding
+	encodedEmbedding, err := embedding.EmbedQuote(quote.Text)
+	if err != nil {
+		b.Session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("Error creating quote embedding.\n```\n%s\n```", err),
+			},
+		})
+		return
+	}
+
+	err = database.Instance.Exec(
+		"UPDATE quote_embeddings SET embedding = ? WHERE rowid = ?",
+		encodedEmbedding,
+		quote.Meta.ID,
+	).Error
+	if err != nil {
+		b.Session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseChannelMessageWithSource,
+			Data: &discordgo.InteractionResponseData{
+				Content: fmt.Sprintf("Error saving quote embedding.\n```\n%s\n```", err),
+			},
+		})
+		return
+	}
 
 	b.Session.InteractionRespond(interaction.Interaction, &discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
