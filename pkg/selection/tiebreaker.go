5b84645353f731aac2e388fc6b3a6f4bfc4b581a
Riley Flynn <riley@rileyflynn.me>
2026-06-29 19:58:12 -0230

    Add more tiebreaker methods

--- pkg/selection/tiebreaker.go
+++ pkg/selection/tiebreaker.go
@@ -5,6 +5,9 @@ import "encoding/gob"
 const (
 	TiebreakerMethodRandom        TiebreakerMethod = "random"
 	TiebreakerMethodHighestRanked TiebreakerMethod = "highest_ranked"
+	TiebreakerMethodLowestRanked  TiebreakerMethod = "lowest_ranked"
+	TiebreakerMethodNewest        TiebreakerMethod = "newest"
+	TiebreakerMethodOldest        TiebreakerMethod = "oldest"
 )
 
 var DefaultTiebreakerMethod = TiebreakerMethodHighestRanked
@@ -12,11 +15,17 @@ var DefaultTiebreakerMethod = TiebreakerMethodHighestRanked
 var TiebreakerMethods = []TiebreakerMethod{
 	TiebreakerMethodRandom,
 	TiebreakerMethodHighestRanked,
+	TiebreakerMethodLowestRanked,
+	TiebreakerMethodNewest,
+	TiebreakerMethodOldest,
 }
 
 var tiebreakerSQL = map[TiebreakerMethod]string{
 	TiebreakerMethodRandom:        "RANDOM()",
 	TiebreakerMethodHighestRanked: "elo DESC",
+	TiebreakerMethodLowestRanked:  "elo ASC",
+	TiebreakerMethodNewest:        "created_at DESC",
+	TiebreakerMethodOldest:        "created_at ASC",
 }
 
 func (m TiebreakerMethod) String() string {
@@ -29,6 +38,12 @@ func (m TiebreakerMethod) DisplayName() string {
 		return "Random"
 	case TiebreakerMethodHighestRanked:
 		return "Highest ranked"
+	case TiebreakerMethodLowestRanked:
+		return "Lowest ranked"
+	case TiebreakerMethodNewest:
+		return "Newest"
+	case TiebreakerMethodOldest:
+		return "Oldest"
 	default:
 		return "Unknown tiebreaker"
 	}
@@ -40,11 +55,21 @@ func (m TiebreakerMethod) Description() string {
 		return "Breaks ties randomly."
 	case TiebreakerMethodHighestRanked:
 		return "When there is a tie, selects the quote with the highest Elo rating."
+	case TiebreakerMethodLowestRanked:
+		return "When there is a tie, selects the quote with the lowest Elo rating."
+	case TiebreakerMethodNewest:
+		return "When there is a tie, selects the newest quote."
+	case TiebreakerMethodOldest:
+		return "When there is a tie, selects the oldest quote."
 	default:
 		return "Unknown tiebreaker"
 	}
 }
 
 func init() {
+	gob.Register(TiebreakerMethodRandom)
 	gob.Register(TiebreakerMethodHighestRanked)
+	gob.Register(TiebreakerMethodLowestRanked)
+	gob.Register(TiebreakerMethodNewest)
+	gob.Register(TiebreakerMethodOldest)
 }
